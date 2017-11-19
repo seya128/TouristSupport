2d0fc5431ad54d2b81f389e58716bc5eb9761877
瀬谷辰宇 <seya@seyatatsusakainoMacBook.local>
2017-11-19 11:40:25 +0900

    場所の検索BOX追加

--- docs/app.js
+++ docs/app.js
@@ -13,23 +13,97 @@ var app = {
             zoom: 14,
             // mapTypeId: 'roadmap'
         });
+        // サーチボックス
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        map.addListener('bounds_changed', function () {
+            searchBox.setBounds(map.getBounds());
+        });
+        var s_markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function () {
+            console.log("places_changed");
+
+            var places = searchBox.getPlaces();
+            console.log(places);
+
+            if (places.length == 0) {
+                return;
+            }
+
+            // Clear out the old markers.
+            s_markers.forEach(function (marker) {
+                marker.setMap(null);
+            });
+            s_markers = [];
+
+            // For each place, get the icon, name and location.
+            var bounds = new google.maps.LatLngBounds();
+            places.forEach(function (place) {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+                var icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                var marker = new google.maps.Marker({
+                    map: map,
+                    icon: icon,
+                    title: place.name,
+                    position: place.geometry.location
+                });
+                marker.addListener( "click", function ( argument ) {
+                    console.log( this ) ;
+                    var point = {
+                        latLng : new google.maps.LatLng(this.position.lat(), this.position.lng()),
+                        title : this.title,
+                    }
+                    app.placeMarker(point, map);
+                }.bind(marker) ) ;
+                s_markers.push(marker);
+
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            map.fitBounds(bounds);
+        });
+
 
         // クリックイベントリスナ
         map.addListener('click', function mylistener(event) {
             console.log(event);
-            var latLng = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
-            this.placeMarker(latLng, map);
+            var point = {
+                latLng : new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()),
+            }
+            this.placeMarker(point, map);
         }.bind(this));
     },
 
     // マーカー追加
-    placeMarker: function (latLng, map) {
+    placeMarker: function (point, map) {
+
         var marker = new google.maps.Marker({
             animation: google.maps.Animation.DROP,
             label: "" + (this.markers.length + 1),
-            position: latLng,
+            position: point.latLng,
             map: map
         });
+        if (point.title) {
+            marker.setTitle(point.title);
+        }
         this.markers.push(marker);
 
         // if(app.markers.length >= 2) {
@@ -50,13 +124,13 @@ new Vue({
     data: function () {
         return app;
     },
-    methods : {
-        getRouteUrl: function(latLng1,latLng2) {
+    methods: {
+        getRouteUrl: function (latLng1, latLng2) {
             var s = "https://www.google.co.jp/maps/dir/";
-            s += latLng1.lat() + "," + latLng1.lng() +"/";
+            s += latLng1.lat() + "," + latLng1.lng() + "/";
             s += latLng2.lat() + "," + latLng2.lng();
             console.log(s);
-            return s;            
+            return s;
         }
     }
 });
