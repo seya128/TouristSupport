320eb8a6c5de736d8a48db107dd93380d171d05f
瀬谷辰宇 <seya@seyatatsusakainoMacBook.local>
2017-11-19 12:38:37 +0900

    名前のついた地点を登録すると、下に名前で表示されるようにした

--- docs/app.js
+++ docs/app.js
@@ -6,15 +6,18 @@ var app = {
     initMap: function () {
         // 初期マーカー位置（JR大阪駅）
         initPos = new google.maps.LatLng(34.702485, 135.495951);
-	// RESAS APIから観光スポット取得
-	var spots = this.getSpots(initPos); 
-	
+        // RESAS APIから観光スポット取得
+        var spots = this.getSpots(initPos);
+
         // マップ生成
         map = new google.maps.Map(document.getElementById('map'), {
             center: initPos,
             zoom: 14,
             // mapTypeId: 'roadmap'
         });
+        // place service
+        this.placeService = new google.maps.places.PlacesService(map);
+
         // サーチボックス
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
@@ -63,14 +66,14 @@ var app = {
                     title: place.name,
                     position: place.geometry.location
                 });
-                marker.addListener( "click", function ( argument ) {
-                    console.log( this ) ;
+                marker.addListener("click", function (argument) {
+                    console.log(this);
                     var point = {
-                        latLng : new google.maps.LatLng(this.position.lat(), this.position.lng()),
-                        title : this.title,
+                        latLng: new google.maps.LatLng(this.position.lat(), this.position.lng()),
+                        title: this.title,
                     }
                     app.placeMarker(point, map);
-                }.bind(marker) ) ;
+                }.bind(marker));
                 s_markers.push(marker);
 
                 if (place.geometry.viewport) {
@@ -88,8 +91,11 @@ var app = {
         map.addListener('click', function mylistener(event) {
             console.log(event);
             var point = {
-                latLng : new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()),
+                latLng: new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()),
             }
+            if (event.placeId)
+                point.placeId = event.placeId;
+
             this.placeMarker(point, map);
         }.bind(this));
     },
@@ -104,42 +110,54 @@ var app = {
             map: map
         });
         if (point.title) {
+            // タイトルがあればそれをセット
             marker.setTitle(point.title);
+            this.markers.push(marker);
+        } else if (point.placeId) {
+            console.log(marker);
+            // place idがあれば、詳細検索して名前をセット
+            request = {
+                placeId: point.placeId,
+            };
+            this.placeService.getDetails(request, function callback(place, status) {
+                console.log(this);
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    this.setTitle(place.name);
+                }
+                app.markers.push(marker);
+            }.bind(marker));
+        } else {
+            this.markers.push(marker);
         }
-        this.markers.push(marker);
-
-        // if(app.markers.length >= 2) {
-        //     app.markers[app.markers.length-2].setLabel("A");
-        // }
     },
 
     // RESAS APIから観光スポット取得
     getSpots: function (pos) {
-	var resasSpotsUrl = 'https://opendata.resas-portal.go.jp/api/v1/tourism/attractions';
-	var rgeoApiUrl = "https://www.finds.jp/ws/rgeocode.php";
-	var apiKey = ""; // RESASのAPI Key
-	$.ajax({
-	    type: 'GET',
-	    url: rgeoApiUrl,
-	    //headers: { 'X-API-KEY': apiKey },
-	    data: {lat: pos.lat(), lon: pos.lng(), json: "1"},
-	    dataType: 'json',
-	    success: function(ret){
-		//console.log(JSON.stringify(ret));
-		var prefCode = ret.result.prefecture.pcode;
-		var cityCode = ret.result.municipality.mcode;
-		console.log("prefCode: "+prefCode);
-		console.log("cityCode: "+cityCode);
-		
-		
-		// FIXME!!
-		
-		
-		//deferred.resolve();
-	    }
-	});
-    }
-
+        var resasSpotsUrl = 'https://opendata.resas-portal.go.jp/api/v1/tourism/attractions';
+        var rgeoApiUrl = "https://www.finds.jp/ws/rgeocode.php";
+        var apiKey = ""; // RESASのAPI Key
+        $.ajax({
+            type: 'GET',
+            url: rgeoApiUrl,
+            //headers: { 'X-API-KEY': apiKey },
+            data: {lat: pos.lat(), lon: pos.lng(), json: "1"},
+            dataType: 'json',
+            success: function(ret){
+            //console.log(JSON.stringify(ret));
+            var prefCode = ret.result.prefecture.pcode;
+            var cityCode = ret.result.municipality.mcode;
+            console.log("prefCode: "+prefCode);
+            console.log("cityCode: "+cityCode);
+            
+            
+            // FIXME!!
+            
+            
+            //deferred.resolve();
+            }
+        });
+        }
+    
 
 };
 
@@ -160,6 +178,15 @@ new Vue({
             s += latLng2.lat() + "," + latLng2.lng();
             console.log(s);
             return s;
+        },
+        getName: function (marker) {
+            var name = "";
+            if (marker.title)
+                name = marker.title;
+            else
+                name = marker.position.lat() + "," + marker.position.lng();
+
+            return name;
         }
     }
 });
